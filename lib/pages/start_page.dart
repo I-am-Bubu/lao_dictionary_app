78ebf05135671afb9f52b34f755b374a486bd334
Thipthida Korsanon <thipthidakorsanon@Thipthidas-MacBook-Air.local>
2023-07-03 21:19:49 +0700

    bottom_nav

--- lib/pages/start_page.dart
+++ lib/pages/start_page.dart
@@ -1,6 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:lao_dictionary_app/pages/main_page.dart';
+import 'package:lao_dictionary_app/widgets/bottom_nav_.dart';
 
 class startPage extends StatefulWidget {
   const startPage({super.key});
